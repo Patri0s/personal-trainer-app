55d5acbe3ddaa80c6f110c659ad16c551d0f0e6e
Patryk <onlylifeplaygames@gmail.com>
2023-06-03 21:34:08 +0200

    Create app splash screen

---
+++ app/src/main/java/pl/patri0s/workoutapp/SplashscreenActivity.kt
@@ -0,0 +1,31 @@
+package pl.patri0s.workoutapp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import pl.patri0s.workoutapp.databinding.ActivitySplashscreenBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashscreenActivity : AppCompatActivity() {
+    private var binding: ActivitySplashscreenBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashscreenBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.appIcon?.animation = AnimationUtils.loadAnimation(this, R.anim.up)
+        binding?.appName?.animation = AnimationUtils.loadAnimation(this, R.anim.down)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            run {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+        }, 2000)
+    }
+}
