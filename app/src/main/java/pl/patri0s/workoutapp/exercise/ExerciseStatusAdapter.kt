7a4cb212086d0bf984f0eced71129ed1c7cc613a
Patryk <onlylifeplaygames@gmail.com>
2023-06-06 15:17:33 +0200

    Add exercise progress bar

---
+++ app/src/main/java/pl/patri0s/workoutapp/exercise/ExerciseStatusAdapter.kt
@@ -0,0 +1,61 @@
+package pl.patri0s.workoutapp.exercise
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import pl.patri0s.workoutapp.R
+import pl.patri0s.workoutapp.databinding.ItemExerciseStatusBinding
+
+class ExerciseStatusAdapter(private val items: List<Exercise>) :
+    RecyclerView.Adapter<ExerciseStatusAdapter.ViewHolder>() {
+    class ViewHolder(binding: ItemExerciseStatusBinding) : RecyclerView.ViewHolder(binding.root) {
+        val tvItem = binding.tvItem
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemExerciseStatusBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val model: Exercise = items[position]
+        holder.tvItem.text = model.getId().toString()
+
+        when {
+            model.getIsSelected() -> {
+                holder.tvItem.background = ContextCompat.getDrawable(
+                    holder.itemView.context,
+                    R.drawable.item_circular_thin_color_accent_border
+                )
+                holder.tvItem.setTextColor(Color.parseColor("#212121"))
+            }
+
+            model.getIsCompleted() -> {
+                holder.tvItem.background = ContextCompat.getDrawable(
+                    holder.itemView.context,
+                    R.drawable.item_circular_color_accent_background
+                )
+                holder.tvItem.setTextColor(Color.parseColor("#FFFFFF"))
+            }
+
+            else -> {
+                holder.tvItem.background = ContextCompat.getDrawable(
+                    holder.itemView.context,
+                    R.drawable.item_circular_color_gray_background
+                )
+                holder.tvItem.setTextColor(Color.parseColor("#212121"))
+            }
+        }
+    }
+}
